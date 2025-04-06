642e0bb82141e0a5f066f5bfa0a027610978fa7f
Daniel Vargas <31797799+Ebriopes@users.noreply.github.com>
2025-04-05 20:06:02 -0600

    :sparkles: feat: update installer script
    The script that will install our dependencies

--- installer.sh
+++ installer.sh
@@ -1,50 +1,88 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# Usamos /usr/bin/env bash para asegurar que se use bash, que es más portable que sh.
 
 clear
 
-os=$(uname -o)
-distro=$(lsb_release -i | cut -f 2-)
+os=$(uname -s)
+distro=$(
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        echo "$ID"
+        elif [ -f /etc/lsb-release ]; then
+        lsb_release -i | cut -d'=' -f2
+    else
+        echo "Unknown"
+    fi
+)
 
-if [ "$os" != "GNU/Linux" ]; then
-	printf "Sorry but I don't know how to manage this OS"
-	exit 1
+if [[ "$os" != "Linux" ]]; then
+    printf "[1;31mError: This script only supports Linux-based operating systems.\n[0m"
+    exit 1
 fi
 
-printf "Operative System: %s\nDistribution: %s\n\n" "$os" "$distro"
+printf "[1;34mOperative System: %s\nDistribution: %s\n\n[0m" "$os" "$distro"
 
-printf "Hi, I'm your dotfiles installer\n\n"
-printf "I will to try install all packages and dependencies need it \nto make your home feel like one \n\n"
+printf "[1;32mHi, I'm your dotfiles installer!\n[0m"
+printf "[1;32mI will try to install all the necessary packages and dependencies to make your home feel like one.\n\n[0m"
 
-if [ "$distro" = "Ubuntu" ]; then
-	missing_packages=""
-	packages="git curl rofi urxvt"
+# Función para manejar la instalación de paquetes, para no repetir código.
+install_packages() {
+    local package_manager="$1"
+    local packages="$2"
+    local missing_packages=""
+    local installed_packages=""
+    
+    printf "[1;33mChecking for required packages (%s)...\n[0m" "$package_manager"
+    
+    # Detectar paquetes ya instalados de la manera correcta para cada gestor.
+    for package in $packages; do
+        printf "  Checking %s...\n" "$package"
+        case "$package_manager" in
+            "apt-get")
+                dpkg -s "$package" >/dev/null 2>&1 || missing_packages+="$package "
+            ;;
+            "pacman")
+                pacman -Q "$package" >/dev/null 2>&1 || missing_packages+="$package "
+            ;;
+            "apk")
+                apk info -e "$package" >/dev/null 2>&1 || missing_packages+="$package "
+            ;;
+            *)
+                printf "[1;31mError: Unsupported package manager: %s\n[0m" "$package_manager"
+                return 1
+            ;;
+        esac
+    done
+    
+    if [ -n "$missing_packages" ]; then
+        printf "[1;33mThe following packages will be installed using %s: \n%s\n\n[0m" "$package_manager" "$missing_packages"
+        # Instalar solo los paquetes faltantes.
+        case "$package_manager" in
+            "apt-get"|"pacman") # Esta es la línea que nos interesa
+                sudo $package_manager -y install $missing_packages
+            ;;
+            "apk")
+                $package_manager add --no-cache $missing_packages
+            ;;
+            *)
+                printf "[1;31mError: Unsupported package manager: %s\n[0m" "$package_manager"
+                return 1
+            ;;
+        esac
+        printf "[1;32mPackages installed successfully!\n[0m"
+    else
+        printf "[1;32mAll required packages for %s are already installed.\n[0m" "$package_manager"
+    fi
+}
 
-	for package in ${packages}; do
-		dpkg -s "${package}" >/dev/null 2>&1 || missing_packages="${missing_packages} ${package}"
-		#printf ${package}
-	done
-
-	printf "%s" "$missing_packages"
+if [ "$distro" = "Ubuntu" ] || [ "$distro" = "Debian" ]; then
+    install_packages "apt-get" "git curl gcc g++ clang make rofi urxvt"
+    elif [ "$distro" = "Manjaro" ] || [ "$distro" = "Arch" ]; then
+    install_packages "pacman" "git curl gcc g++ clang make rofi urxvt"
+    elif [ "$distro" = "alpine" ]; then
+    install_packages "apk" "git curl gcc g++ clang make rofi rxvt-unicode"
 fi
 
-if [ "$distro" = "ManjaroLinux" ]; then
-	missing_packages=""
-	packages="git curl rofi urxvt"
+printf "[1;34mRunning original installer script...\n[0m"
 
-	printf "The next packages will be installed: \n%s\n\n" "${packages}"
-
-	#sudo pacman -S ${packages} >/dev/null 2>&1 || missing_packages="${missing_packages} ${packages}"
-
-	sudo pacman -S ${packages}
-
-	#printf "\n\n"
-
-	missing_packages=$(pacman -Qi ${packages} 2>&1 >/dev/null)
-
-	missing_packages=$(echo "${missing_packages}" | awk '{print $4}')
-
-	printf "\n\nMissing packages: \n%s\n\n" "${missing_packages}"
-
-
-fi
-
+bash ${PWD}/dotfiles/original-installer.sh # Ejecuta el script original
